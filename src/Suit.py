bab4697ebcacd4ecfa2da6a05f3811d9999f733e
firedial <42674772+firedial@users.noreply.github.com>
2023-07-23 14:42:28 +0900

    名前の変更

--- src/Suit.py
+++ src/Suit.py
@@ -4,51 +4,49 @@ from typing import ClassVar, Optional
 @dataclass(frozen=True)
 class Suit:
 
-    hand: list[int]
     suit: list[int]
     MAX_TILE_COUNT: ClassVar[int] = 4
-    HAND_LENGTH: ClassVar[int] = 9
+    SUIT_LENGTH: ClassVar[int] = 9
 
-    def __init__(self, hand: list[int]):
-        if len(hand) != self.HAND_LENGTH:
-            raise ValueError("Wrong hand length.")
+    def __init__(self, suit: list[int]):
+        if len(suit) != self.SUIT_LENGTH:
+            raise ValueError("Wrong suit length.")
 
-        for tile in hand:
+        for tile in suit:
             if tile > self.MAX_TILE_COUNT or tile < 0:
                 raise ValueError("Wrong tile count.")
 
-        object.__setattr__(self, "hand", hand)
-        object.__setattr__(self, "suit", hand)
+        object.__setattr__(self, "suit", suit)
 
     def length(self) -> int:
-        return len(self.hand)
+        return len(self.suit)
 
     def sum(self) -> int:
-        return sum(self.hand)
+        return sum(self.suit)
 
     def isRegularForm(self) -> bool:
         return self.sum() % 3 == 1
 
     def isFirstTIleZero(self) -> bool:
-        return self.hand [0] == 0
+        return self.suit[0] == 0
 
     def getRemainTileCount(self, index: int) -> int:
-        if index < 0 or index >= self.HAND_LENGTH:
+        if index < 0 or index >= self.SUIT_LENGTH:
             raise ValueError("Invalid index.")
 
-        return self.MAX_TILE_COUNT - self.hand[index]
+        return self.MAX_TILE_COUNT - self.suit[index]
 
     # @todo 返り値の型を付ける
     def addTile(self, index: int):
-        if index < 0 or index >= self.HAND_LENGTH:
+        if index < 0 or index >= self.SUIT_LENGTH:
             raise ValueError("Invalid index.")
 
-        resultHand = self.hand.copy()
-        resultHand[index] += 1
-        if resultHand[index] > self.MAX_TILE_COUNT:
+        resultSuit = self.suit.copy()
+        resultSuit[index] += 1
+        if resultSuit[index] > self.MAX_TILE_COUNT:
             return None
 
-        return Suit(resultHand)
+        return Suit(resultSuit)
 
 
     def isBasicForm(self) -> bool:
@@ -62,39 +60,36 @@ class Suit:
             4. 2 番目から始まっていたら基本形
             5. それ以外は基本形ではない
 
-        Args:
-            hand (lsit[int]): 牌形
-
         Returns:
             bool: 基本形であれば True / そうでないとき False
         """
 
-        hand = self.hand.copy()
-        gravity: int = self.getHandGravityPosition()
+        suit = self.suit.copy()
+        gravity: int = self.getSuitGravityPosition()
 
         # 後方重心なら基本形ではない
         if gravity == -1:
             return False
 
         # 両接地なら基本形
-        if hand[0] != 0 and hand[-1] != 0:
+        if suit[0] != 0 and suit[-1] != 0:
             return True
 
         # ゆとりが 1 の時も基本形
-        if (hand[0] == 0 and hand[1] != 0 and hand[-1] != 0) or (hand[0] != 0 and hand[-2] != 0 and hand[-1] == 0):
+        if (suit[0] == 0 and suit[1] != 0 and suit[-1] != 0) or (suit[0] != 0 and suit[-2] != 0 and suit[-1] == 0):
             return True
 
         # 2番目から始まっていた場合基本形
-        if hand[0] == 0 and hand[1] != 0:
+        if suit[0] == 0 and suit[1] != 0:
             return True
 
         # それ以外は基本形ではない
         return False
 
 
-    def getHandGravityPosition(self) -> int:
+    def getSuitGravityPosition(self) -> int:
         """
-        牌形の重心を求める
+        数牌の重心を求める
 
         両端に接地している連続する 0 を無視する。
         両端から見て行った時、大きい数字がある方に重心がある。
@@ -106,37 +101,34 @@ class Suit:
             [0, 2, 2, 1, 2] -> 前方重心
             [0, 2, 1, 1, 2, 0, 0] -> 左右対称形
 
-        Args:
-            hand (list[int]): 牌形
-
         Returns:
             int: 前方重心 1 / 左右対称 0 / 後方重心 -1
 
         """
-        handLength: int = self.length()
-        hand = self.hand.copy()
+        suitLength: int = self.length()
+        suit = self.suit.copy()
 
         first: int = 0
-        last: int = handLength - 1
+        last: int = suitLength - 1
 
         # 前から見たときに初めて 0 でない場所を見つける
-        while first < handLength:
-            if hand[first] != 0:
+        while first < suitLength:
+            if suit[first] != 0:
                 break
             first += 1
 
         # 後ろから見たときに初めて 0 でない場所を見つける
         while 0 <= last:
-            if hand[last] != 0:
+            if suit[last] != 0:
                 break
             last -= 1
 
         # 両端から重心を見ていく
         while first < last:
-            if hand[first] > hand[last]:
+            if suit[first] > suit[last]:
                 # 前方重心
                 return 1
-            if hand[first] < hand[last]:
+            if suit[first] < suit[last]:
                 # 後方重心
                 return -1
             first += 1
