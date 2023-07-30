0fb2778a30a0828351036e2a760c582900aaad57
firedial <42674772+firedial@users.noreply.github.com>
2023-07-30 18:48:06 +0900

    ちょっとリファクタ

--- src/WaitingPattern.py
+++ src/WaitingPattern.py
@@ -2,11 +2,11 @@ from src.Hand import Hand
 import src.SuitLoop as SuitLoop
 
 
-def setWaitingPatern(waitingPattern: dict, waitingNumber: int, suit):
-    waitingPattern[tuple(suit.suit)] = {"suit": suit.suit, "number": waitingNumber, "length": suit.getRange(), "position": suit.getPosition(), "gravity": suit.getSuitGravityPosition()}
+def setWaitingPatern(waitingPatterns: dict, waitingNumber: int, suit):
+    waitingPatterns[tuple(suit.suit)] = {"suit": suit.suit, "number": waitingNumber, "position": suit.getPosition(), "gravity": suit.getSuitGravityPosition()}
 
 
-def getWaitingPattern(waitingPattern: dict, number: int):
+def getWaitingPattern(waitingPatterns: dict, number: int):
     suit = SuitLoop.getFirstSuit(number)
     firstSuit = suit
 
@@ -24,15 +24,15 @@ def getWaitingPattern(waitingPattern: dict, number: int):
             # 右接地パターン
             hand = Hand(suit)
             if hand.isTempai() and hand.isIrreducible():
-                setWaitingPatern(waitingPattern, waitingNumber, suit)
-                setWaitingPatern(waitingPattern, waitingNumber, suit.getReverseSuit())
+                setWaitingPatern(waitingPatterns, waitingNumber, suit)
+                setWaitingPatern(waitingPatterns, waitingNumber, suit.getReverseSuit())
 
             # 左接地パターン
             leftAtattchSuit = suit.getOneLeftSuit()
             hand = Hand(leftAtattchSuit)
             if hand.isTempai() and hand.isIrreducible():
-                setWaitingPatern(waitingPattern, waitingNumber, leftAtattchSuit)
-                setWaitingPatern(waitingPattern, waitingNumber, leftAtattchSuit.getReverseSuit())
+                setWaitingPatern(waitingPatterns, waitingNumber, leftAtattchSuit)
+                setWaitingPatern(waitingPatterns, waitingNumber, leftAtattchSuit.getReverseSuit())
             continue
 
         # 聴牌形、既約系でなければ考慮外
@@ -42,8 +42,8 @@ def getWaitingPattern(waitingPattern: dict, number: int):
 
         # 範囲が9の時はその形だけを登録
         if suit.getRange() == 9:
-            setWaitingPatern(waitingPattern, waitingNumber, suit)
-            setWaitingPatern(waitingPattern, waitingNumber, suit.getReverseSuit())
+            setWaitingPatern(waitingPatterns, waitingNumber, suit)
+            setWaitingPatern(waitingPatterns, waitingNumber, suit.getReverseSuit())
             continue
 
         # 範囲が7以下のときは、移動系と右接地と左接地について見る
@@ -51,8 +51,8 @@ def getWaitingPattern(waitingPattern: dict, number: int):
         setSuit = suit.getOneLeftSuit()
         for _ in range(8 - suit.getRange()):
             setSuit = setSuit.getOneRightSuit()
-            setWaitingPatern(waitingPattern, waitingNumber, setSuit)
-            setWaitingPatern(waitingPattern, waitingNumber, setSuit.getReverseSuit())
+            setWaitingPatern(waitingPatterns, waitingNumber, setSuit)
+            setWaitingPatern(waitingPatterns, waitingNumber, setSuit.getReverseSuit())
 
             continue
 
@@ -61,36 +61,34 @@ def getWaitingPattern(waitingPattern: dict, number: int):
         setSuit = suit.getOneLeftSuit()
         for _ in range(8 - suit.getRange()):
             setSuit = setSuit.getOneRightSuit()
-            setWaitingPatern(waitingPattern, waitingNumber, setSuit)
-            setWaitingPatern(waitingPattern, waitingNumber, setSuit.getReverseSuit())
+            setWaitingPatern(waitingPatterns, waitingNumber, setSuit)
+            setWaitingPatern(waitingPatterns, waitingNumber, setSuit.getReverseSuit())
 
         # 左接地は聴牌形で既約形かどうかをみる
         leftAtattchSuit = suit.getOneLeftSuit()
         hand = Hand(leftAtattchSuit)
         if hand.isTempai() and hand.isIrreducible():
-            setWaitingPatern(waitingPattern, waitingNumber, leftAtattchSuit)
-            setWaitingPatern(waitingPattern, waitingNumber, leftAtattchSuit.getReverseSuit())
+            setWaitingPatern(waitingPatterns, waitingNumber, leftAtattchSuit)
+            setWaitingPatern(waitingPatterns, waitingNumber, leftAtattchSuit.getReverseSuit())
 
         # 右接地も聴牌形で既約形かどうかをみる
         rightAttachSuit = suit.getRightAttachSuit()
         hand = Hand(rightAttachSuit)
         if hand.isTempai() and hand.isIrreducible():
-            setWaitingPatern(waitingPattern, waitingNumber, rightAttachSuit)
-            setWaitingPatern(waitingPattern, waitingNumber, rightAttachSuit.getReverseSuit())
-
-    return waitingPattern
+            setWaitingPatern(waitingPatterns, waitingNumber, rightAttachSuit)
+            setWaitingPatern(waitingPatterns, waitingNumber, rightAttachSuit.getReverseSuit())
 
 def main():
-    waitingPattern = {}
-    getWaitingPattern(waitingPattern, 1)
-    getWaitingPattern(waitingPattern, 2)
-    getWaitingPattern(waitingPattern, 4)
-    getWaitingPattern(waitingPattern, 5)
-    getWaitingPattern(waitingPattern, 7)
-    getWaitingPattern(waitingPattern, 8)
-    getWaitingPattern(waitingPattern, 10)
-    getWaitingPattern(waitingPattern, 11)
-    getWaitingPattern(waitingPattern, 13)
+    waitingPatterns = {}
+    getWaitingPattern(waitingPatterns, 1)
+    getWaitingPattern(waitingPatterns, 2)
+    getWaitingPattern(waitingPatterns, 4)
+    getWaitingPattern(waitingPatterns, 5)
+    getWaitingPattern(waitingPatterns, 7)
+    getWaitingPattern(waitingPatterns, 8)
+    getWaitingPattern(waitingPatterns, 10)
+    getWaitingPattern(waitingPatterns, 11)
+    getWaitingPattern(waitingPatterns, 13)
 
-    return waitingPattern
+    return waitingPatterns
 
--- waitingPattern.py
+++ waitingPattern.py
@@ -2,6 +2,6 @@ import src.WaitingPattern as WaitingPattern
 
 result = WaitingPattern.main()
 f = open('result/waitingPattern.py', 'w')
-f.write("def getWaitingPattern():\n\treturn ")
-print(result, file = f)
+f.write("def getWaitingPatterns():\n\treturn ")
+print(list(result.values()), file = f)
 f.close()
