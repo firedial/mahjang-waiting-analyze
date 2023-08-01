924268a952813d369dd56e0e0315afb68232fa2d
firedial <42674772+firedial@users.noreply.github.com>
2023-08-02 00:16:14 +0900

    追加し忘れ

--- waitingPattern.py
+++ waitingPattern.py
@@ -1,7 +1,11 @@
 import src.WaitingPattern as WaitingPattern
 
 result = WaitingPattern.main()
+
 f = open('result/waitingPattern.py', 'w')
-f.write("def getWaitingPatterns():\n\treturn ")
-print(list(result.values()), file = f)
+f.write("def getWaitingPatterns():\n    return [\n")
+for waiting in result:
+    print("        ", file = f, end = "")
+    print(waiting, file = f, end = ",\n")
+f.write("    ]")
 f.close()
