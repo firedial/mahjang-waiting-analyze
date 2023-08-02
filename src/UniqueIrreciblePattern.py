c2e237be911ac93f0b2ced6a136d8283707f86c3
firedial <42674772+firedial@users.noreply.github.com>
2023-08-02 21:57:20 +0900

    リスト修正に伴って実装修正

--- src/UniqueIrreciblePattern.py
+++ src/UniqueIrreciblePattern.py
@@ -4,16 +4,47 @@ import src.Remove as Remove
 import src.SuitLoop as SuitLoop
 import result.waitingPattern
 
-def getWaitingPatterns() -> dict:
-    waitingPatterns = {}
-    for waiting in result.waitingPattern.getWaitingPatterns():
-        waitingPatterns[waiting["suit"]] = waiting
+def getAllWaitingPatterns() -> dict:
+    waitingPatterns = result.waitingPattern.getWaitingPatterns()
+    allWaitingPatterns = {}
+    for waitingPattern in waitingPatterns:
+        suit = Suit(waitingPattern["suit"])
 
-    return waitingPatterns
+        if suit.getRange() == 9:
+            allWaitingPatterns[(suit.suit, waitingPattern["isAcs"])] = {"suit": suit.suit, "isAcs": waitingPattern["isAcs"], "number": waitingPattern["number"]}
+            allWaitingPatterns[(suit.getReverseSuit().suit, waitingPattern["isAcs"])] = {"suit": suit.getReverseSuit().suit, "isAcs": waitingPattern["isAcs"], "number": waitingPattern["number"]}
+        elif suit.getRange() == 8:
+            if waitingPattern["right"]:
+                allWaitingPatterns[(suit.suit, waitingPattern["isAcs"])] = {"suit": suit.suit, "isAcs": waitingPattern["isAcs"], "number": waitingPattern["number"]}
+                allWaitingPatterns[(suit.getReverseSuit().suit, waitingPattern["isAcs"])] = {"suit": suit.getReverseSuit().suit, "isAcs": waitingPattern["isAcs"], "number": waitingPattern["number"]}
+            if waitingPattern["left"]:
+                suit = suit.getOneLeftSuit()
+                allWaitingPatterns[(suit.suit, waitingPattern["isAcs"])] = {"suit": suit.suit, "isAcs": waitingPattern["isAcs"], "number": waitingPattern["number"]}
+                allWaitingPatterns[(suit.getReverseSuit().suit, waitingPattern["isAcs"])] = {"suit": suit.getReverseSuit().suit, "isAcs": waitingPattern["isAcs"], "number": waitingPattern["number"]}
+        else:
+            suit = suit.getOneLeftSuit()
+            if waitingPattern["left"]:
+                allWaitingPatterns[(suit.suit, waitingPattern["isAcs"])] = {"suit": suit.suit, "isAcs": waitingPattern["isAcs"], "number": waitingPattern["number"]}
+                allWaitingPatterns[(suit.getReverseSuit().suit, waitingPattern["isAcs"])] = {"suit": suit.getReverseSuit().suit, "isAcs": waitingPattern["isAcs"], "number": waitingPattern["number"]}
 
-def getIrreducibles(suit: Suit, waitingPatterns: dict):
-    if suit.suit in waitingPatterns:
-        return set((suit.suit, ))
+            for _ in range(8 - suit.getRange()):
+                suit = suit.getOneRightSuit()
+                allWaitingPatterns[(suit.suit, waitingPattern["isAcs"])] = {"suit": suit.suit, "isAcs": waitingPattern["isAcs"], "number": waitingPattern["number"]}
+                allWaitingPatterns[(suit.getReverseSuit().suit, waitingPattern["isAcs"])] = {"suit": suit.getReverseSuit().suit, "isAcs": waitingPattern["isAcs"], "number": waitingPattern["number"]}
+
+            suit = suit.getOneRightSuit()
+            if waitingPattern["right"]:
+                allWaitingPatterns[(suit.suit, waitingPattern["isAcs"])] = {"suit": suit.suit, "isAcs": waitingPattern["isAcs"], "number": waitingPattern["number"]}
+                allWaitingPatterns[(suit.getReverseSuit().suit, waitingPattern["isAcs"])] = {"suit": suit.getReverseSuit().suit, "isAcs": waitingPattern["isAcs"], "number": waitingPattern["number"]}
+
+    return allWaitingPatterns
+
+def getIrreducibles(suit: Suit, allWaitingPatterns: dict):
+    if (suit.suit, False) in allWaitingPatterns:
+        t = tuple((suit.suit, False))
+        s = set()
+        s.add(t)
+        return s
 
     hand = Hand(suit, False)
     result = set()
@@ -24,22 +55,22 @@ def getIrreducibles(suit: Suit, waitingPatterns: dict):
     if suit.isRegularForm() and suit.sum() >= 10:
         suits = Remove.getRemovedAtamaConnectedShuntsuPatterns(suit)
         for checkSuit in suits:
-            checkHand = Hand(checkSuit, False)
+            checkHand = Hand(checkSuit, True)
             if not checkHand < hand:
-                result |= getIrreducibles(checkSuit, waitingPatterns)
+                result |= getIrreducibles(checkSuit, allWaitingPatterns)
 
     if suit.isRegularForm():
         suits = Remove.getRemovedAtamaPatterns(suit)
         for checkSuit in suits:
-            checkHand = Hand(checkSuit, False)
+            checkHand = Hand(checkSuit)
             if not checkHand < hand:
-                result |= getIrreducibles(checkSuit, waitingPatterns)
+                result |= getIrreducibles(checkSuit, allWaitingPatterns)
 
     suits = Remove.getRemovedMentsuPatterns(suit)
     for checkSuit in suits:
-        checkHand = Hand(checkSuit, False)
+        checkHand = Hand(checkSuit)
         if not checkHand < hand:
-            result |= getIrreducibles(checkSuit, waitingPatterns)
+            result |= getIrreducibles(checkSuit, allWaitingPatterns)
 
     return result
 
@@ -48,13 +79,17 @@ def checkIrrecible(number: int):
     suit = SuitLoop.getFirstSuit(number)
     firstSuit = suit
 
-    waitingPatterns = getWaitingPatterns()
+    allWaitingPatterns = getAllWaitingPatterns()
     irreducibleWaitings = {}
 
-    while (suit := SuitLoop.nextSuit(suit)) != firstSuit:
-        irreducibles = getIrreducibles(suit, waitingPatterns)
-        if len(set(map(lambda x: waitingPatterns[x]['number'], irreducibles))) > 1:
+    while True:
+        irreducibles = getIrreducibles(suit, allWaitingPatterns)
+        if len(set(map(lambda x: allWaitingPatterns[x]['number'], irreducibles))) > 0:
             irreducibleWaitings[suit.suit] = irreducibles
+
+        suit = SuitLoop.nextSuit(suit)
+        if suit == firstSuit:
+            break
 
     return irreducibleWaitings
 
