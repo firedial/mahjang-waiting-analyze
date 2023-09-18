2fb788df942411740ef6ccfe57d763e1b75a761a
firedial <42674772+firedial@users.noreply.github.com>
2023-09-18 20:16:42 +0900

    ループ処理を切り出した

--- src/WaitingPattern.py
+++ src/WaitingPattern.py
@@ -13,75 +13,67 @@ def setWaitingNumber(waitingPatterns: list) -> list:
     return sortedPatterns
 
 
-def getWaitingPatterns(waitingPatterns: list, number: int):
+def getWaitingPatterns(suit: Suit):
+    # 基本形ではない時は考慮外
+    if not suit.isBasicForm():
+        return None
+
+    # 両接地形
+    if suit.isRangeFull():
+        # 聴牌形、既約系でなければ考慮外
+        if not (suit.isTempai() and suit.isIrreducible()):
+            return None
+
+        isRightIrreducible = True
+        isLeftIrreducible = True
+
+    # 片接地形
+    elif suit.hasOneRoomRange():
+        # 右接地パターン
+        rightAttachSuit = suit
+        isRightIrreducible = rightAttachSuit.isTempai() and rightAttachSuit.isIrreducible()
+
+        # 左接地パターン
+        leftAttachSuit = suit.getLeftAttachSuit()
+        isLeftIrreducible = leftAttachSuit.isTempai() and leftAttachSuit.isIrreducible()
+
+        # どっちに接地していても既約でない場合は登録しない
+        if (not isRightIrreducible) and (not isLeftIrreducible):
+            return None
+
+    else:
+        # 範囲が7以下のときは、移動系と右接地と左接地について見る
+
+        # 無接地パターン
+        isCenterIrreducible = suit.isTempai() and suit.isIrreducible()
+
+        # 右接地パターン
+        rightAttachSuit = suit.getRightAttachSuit()
+        isRightIrreducible = rightAttachSuit.isTempai() and rightAttachSuit.isIrreducible()
+
+        # 左接地パターン
+        leftAttachSuit = suit.getLeftAttachSuit()
+        isLeftIrreducible = leftAttachSuit.isTempai() and leftAttachSuit.isIrreducible()
+
+        # 無接地は既約じゃないが、接地パターンが既約になる場合はない想定
+        if not isCenterIrreducible and (isRightIrreducible or isLeftIrreducible):
+            raise RuntimeError("Unexpected error.")
+
+        # 既約でない場合は登録しない
+        if not isCenterIrreducible:
+            return None
+
+    return {"suit": suit.suit, "left": isLeftIrreducible, "right": isRightIrreducible, "isSendable": suit.isSendable(), "suitNumber": 1}
+
+
+def getWaitingPatternsLoop(waitingPatterns: list, number: int):
     suit = SuitLoop.getFirstSuit(number)
     firstSuit = suit
 
     while True:
-        # 基本形ではない時は考慮外
-        if not suit.isBasicForm():
-            suit = SuitLoop.nextSuit(suit)
-            if suit == firstSuit:
-                break
-
-            continue
-
-        if suit.isRangeFull():
-            # 聴牌形、既約系でなければ考慮外
-            if not (suit.isTempai() and suit.isIrreducible()):
-                suit = SuitLoop.nextSuit(suit)
-                if suit == firstSuit:
-                    break
-
-                continue
-
-            isRightIrreducible = True
-            isLeftIrreducible = True
-
-        elif suit.hasOneRoomRange():
-            # 右接地パターン
-            rightAttachSuit = suit
-            isRightIrreducible = rightAttachSuit.isTempai() and rightAttachSuit.isIrreducible()
-
-            # 左接地パターン
-            leftAttachSuit = suit.getLeftAttachSuit()
-            isLeftIrreducible = leftAttachSuit.isTempai() and leftAttachSuit.isIrreducible()
-
-            # どっちに接地していても既約でない場合は登録しない
-            if (not isRightIrreducible) and (not isLeftIrreducible):
-                suit = SuitLoop.nextSuit(suit)
-                if suit == firstSuit:
-                    break
-
-                continue
-
-        else:
-            # 範囲が7以下のときは、移動系と右接地と左接地について見る
-
-            # 無接地パターン
-            isCenterIrreducible = suit.isTempai() and suit.isIrreducible()
-
-            # 右接地パターン
-            rightAttachSuit = suit.getRightAttachSuit()
-            isRightIrreducible = rightAttachSuit.isTempai() and rightAttachSuit.isIrreducible()
-
-            # 左接地パターン
-            leftAttachSuit = suit.getLeftAttachSuit()
-            isLeftIrreducible = leftAttachSuit.isTempai() and leftAttachSuit.isIrreducible()
-
-            # 無接地は既約じゃないが、接地パターンが既約になる場合はない想定
-            if not isCenterIrreducible and (isRightIrreducible or isLeftIrreducible):
-                raise RuntimeError("Unexpected error.")
-
-            # 既約でない場合は登録しない
-            if not isCenterIrreducible:
-                suit = SuitLoop.nextSuit(suit)
-                if suit == firstSuit:
-                    break
-
-                continue
-
-        waitingPatterns.append({"suit": suit.suit, "left": isLeftIrreducible, "right": isRightIrreducible, "isSendable": suit.isSendable(), "suitNumber": 1})
+        waitingPattern = getWaitingPatterns(suit)
+        if waitingPattern is not None:
+            waitingPatterns.append(waitingPattern)
 
         suit = SuitLoop.nextSuit(suit)
         if suit == firstSuit:
@@ -89,16 +81,15 @@ def getWaitingPatterns(waitingPatterns: list, number: int):
 
 
 def main():
-    isSameWaiting = lambda x: True
     waitingPatterns = []
-    getWaitingPatterns(waitingPatterns, 1)
-    getWaitingPatterns(waitingPatterns, 2)
-    getWaitingPatterns(waitingPatterns, 4)
-    getWaitingPatterns(waitingPatterns, 5)
-    getWaitingPatterns(waitingPatterns, 7)
+    getWaitingPatternsLoop(waitingPatterns, 1)
+    getWaitingPatternsLoop(waitingPatterns, 2)
+    getWaitingPatternsLoop(waitingPatterns, 4)
+    getWaitingPatternsLoop(waitingPatterns, 5)
+    getWaitingPatternsLoop(waitingPatterns, 7)
     return setWaitingNumber(waitingPatterns)
-    getWaitingPatterns(waitingPatterns, 8)
-    getWaitingPatterns(waitingPatterns, 10)
-    getWaitingPatterns(waitingPatterns, 11)
-    getWaitingPatterns(waitingPatterns, 13)
+    getWaitingPatternsLoop(waitingPatterns, 8)
+    getWaitingPatternsLoop(waitingPatterns, 10)
+    getWaitingPatternsLoop(waitingPatterns, 11)
+    getWaitingPatternsLoop(waitingPatterns, 13)
     return setWaitingNumber(waitingPatterns)
