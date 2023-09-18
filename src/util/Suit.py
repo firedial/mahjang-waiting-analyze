8c12c33ae41b67cf3eb9559916304a69903af035
firedial <42674772+firedial@users.noreply.github.com>
2023-09-18 17:18:52 +0900

    和了に関する処理を追加

--- src/util/Suit.py
+++ src/util/Suit.py
@@ -1,6 +1,9 @@
 from dataclasses import dataclass
 from typing import ClassVar
 from src.util.Block import Block
+from functools import cached_property
+from src.util.WaitingType import WaitingType
+from src.util.WaitingStructure import WaitingStructure
 from typing import Self
 
 
@@ -24,11 +27,14 @@ class Suit:
     def length(self) -> int:
         return len(self.suit)
 
+    def sum(self) -> int:
+        return sum(self.suit)
+
     # ---------------------------------------------------------------------------- #
     # 牌除去に関する処理 ここから
     # ---------------------------------------------------------------------------- #
 
-    def getRemovedBlockPatterns(self, block: Block) -> list[Self]:
+    def __getRemovedBlockPatterns(self, block: Block) -> list[Self]:
         """
         数牌から牌を除去できるパターンのリストを返す
 
@@ -59,7 +65,7 @@ class Suit:
         return removedSuits
 
 
-    def getRemovedMultiBlockPatterns(self, blocks: list[Block]) -> list[Self]:
+    def __getRemovedMultiBlockPatterns(self, blocks: list[Block]) -> list[Self]:
         """
         数牌から牌を除去できるパターンのリストを返す
 
@@ -73,7 +79,7 @@ class Suit:
         removedSuits: list[Self] = []
 
         for block in blocks:
-            removedSuits += self.getRemovedBlockPatterns(block)
+            removedSuits += self.__getRemovedBlockPatterns(block)
 
         return removedSuits
 
@@ -85,7 +91,7 @@ class Suit:
         Returns:
             lits[Self]: 数牌から面子のパターンを省いた牌形のリスト
         """
-        return self.getRemovedBlockPatterns(Block((2, )))
+        return self.__getRemovedBlockPatterns(Block((2, )))
 
 
     def getRemovedAtamaConnectedShuntsuPatterns(self) -> list[Self]:
@@ -95,7 +101,7 @@ class Suit:
         Returns:
             lits[Self]: 牌形から雀頭接続順子のパターンを省いた数牌のリスト
         """
-        return self.getRemovedMultiBlockPatterns([Block((3, 1, 1, 0)), Block((0, 1, 1, 3))])
+        return self.__getRemovedMultiBlockPatterns([Block((3, 1, 1, 0)), Block((0, 1, 1, 3))])
 
 
     def getRemovedMentsuPatterns(self) -> list[Self]:
@@ -105,9 +111,123 @@ class Suit:
         Returns:
             lits[Self]: 牌形から面子のパターンを省いた数牌のリスト
         """
-        return self.getRemovedMultiBlockPatterns([Block((3, )), Block((1, 1, 1))])
+        return self.__getRemovedMultiBlockPatterns([Block((3, )), Block((1, 1, 1))])
 
     # ---------------------------------------------------------------------------- #
     # 牌除去に関する処理 ここまで
+    # ---------------------------------------------------------------------------- #
+
+    # ---------------------------------------------------------------------------- #
+    # 和了に関する処理 ここから
+    # ---------------------------------------------------------------------------- #
+
+    def __getTankiJudgeSuit(self, index: int) -> Self:
+        if index < 0 or index >= self.SUIT_LENGTH:
+            return None
+
+        if self.suit[index] == 0:
+            return None
+
+        return Suit(self.suit[:index] + (self.suit[index] - 1, ) + self.suit[index + 1:])
+
+    def __getShamponJudgeSuit(self, index: int) -> Self:
+        if index < 0 or index >= self.SUIT_LENGTH:
+            return None
+
+        if self.suit[index] < 2:
+            return None
+
+        return Suit(self.suit[:index] + (self.suit[index] - 2, ) + self.suit[index + 1:])
+
+    def __getKanchanJudgeSuit(self, index: int) -> Self:
+        if index < 1 or index >= self.SUIT_LENGTH - 1:
+            return None
+
+        if self.suit[index - 1] == 0 or self.suit[index + 1] == 0:
+            return None
+
+        return Suit(self.suit[:index - 1] + (self.suit[index - 1] - 1, self.suit[index], self.suit[index + 1] - 1) + self.suit[index + 2:])
+
+    def __getRyanmenLeftJudgeSuit(self, index: int) -> Self:
+        if index < 0 or index >= self.SUIT_LENGTH - 2:
+            return None
+
+        if self.suit[index + 1] == 0 or self.suit[index + 2] == 0:
+            return None
+
+        return Suit(self.suit[:index] + (self.suit[index], self.suit[index + 1] - 1, self.suit[index + 2] - 1) + self.suit[index + 3:])
+
+    def __getRyanmenRightJudgeSuit(self, index: int) -> Self:
+        if index < 2 or index >= self.SUIT_LENGTH:
+            return None
+
+        if self.suit[index - 1] == 0 or self.suit[index - 2] == 0:
+            return None
+
+        return Suit(self.suit[:index - 2] + (self.suit[index - 2] - 1, self.suit[index - 1] - 1, self.suit[index]) + self.suit[index + 1:])
+
+    def __isAgari(self) -> bool:
+        """
+        渡された数牌が和了形であるかどうかを判定する
+
+        牌の枚数によって判定条件が変わる
+            3n + 2 枚のとき: 1 雀頭 n 面子のときに限り和了形
+            3n + 1 枚のとき: 和了形にならない
+            3n 枚のとき: n 面子のときに限り和了形
+
+        Returns:
+            bool: 和了形であれば True / そうでないとき False
+
+        """
+        # 手牌の牌の合計枚数
+        suitTileCount: int = self.sum()
+
+        # 手牌の合計枚数が0なら和了形
+        if suitTileCount == 0:
+            return True
+
+        # 枚数が 3n + 1 場合は和了形にならない
+        if suitTileCount % 3 == 1:
+            return False
+
+        # 枚数が 3n + 2 場合は頭を除去する
+        if suitTileCount % 3 == 2:
+            for h in self.getRemovedAtamaPatterns():
+                if h.__isAgari():
+                    return True
+            else:
+                return False
+
+        # 3n 枚の時のは面子の除去
+        for h in self.getRemovedMentsuPatterns():
+            if h.__isAgari():
+                return True
+        else:
+            return False
+
+    @cached_property
+    def waitingStructure(self) -> WaitingStructure:
+        waitingStructures = []
+        for index in range(self.length()):
+            tankiJudgeSuit = self.__getTankiJudgeSuit(index)
+            shamponJudgeSuit = self.__getShamponJudgeSuit(index)
+            kanchanJudgeSuit = self.__getKanchanJudgeSuit(index)
+            ryanmenLeftJudgeSuit = self.__getRyanmenLeftJudgeSuit(index)
+            ryanmenRightJudgeSuit = self.__getRyanmenRightJudgeSuit(index)
+
+            waitingStructures.append(
+                WaitingType(
+                    isTanki = False if tankiJudgeSuit is None else tankiJudgeSuit.__isAgari(),
+                    isShampon = False if shamponJudgeSuit is None else shamponJudgeSuit.__isAgari(),
+                    isKanchan = False if kanchanJudgeSuit is None else kanchanJudgeSuit.__isAgari(),
+                    isRyanmenLeft = False if ryanmenLeftJudgeSuit is None else ryanmenLeftJudgeSuit.__isAgari(),
+                    isRyanmenRight = False if ryanmenRightJudgeSuit is None else ryanmenRightJudgeSuit.__isAgari(),
+                )
+            )
+
+        return WaitingStructure(tuple(waitingStructures))
+
+    # ---------------------------------------------------------------------------- #
+    # 和了に関する処理 ここまで
     # ---------------------------------------------------------------------------- #
 
