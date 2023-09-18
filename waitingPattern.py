5a7d7629423f5badbd3b88b27de1570b57e03a82
firedial <42674772+firedial@users.noreply.github.com>
2023-09-19 05:53:10 +0900

    差分がわかりやすいように牌番号を削除

--- waitingPattern.py
+++ waitingPattern.py
@@ -7,9 +7,9 @@ result = WaitingPattern.main()
 csvForm = []
 for value in result:
     value['suit'] = ''.join(map(lambda x: str(x), value['suit']))
+    value.pop("number")
     csvForm.append(value)
 
 with open('result/waitingPattern.csv', 'w') as f:
-    writer = csv.DictWriter(f, ['number', 'suit', 'left', 'right'])
-    writer.writeheader()
+    writer = csv.DictWriter(f, ['suit', 'left', 'right'])
     writer.writerows(csvForm)
