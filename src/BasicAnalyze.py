e0234648d108852ebcf03b7a6deea6b02580a331
firedial <42674772+firedial@users.noreply.github.com>
2023-09-20 11:06:34 +0900

    基本的な解析追加

--- src/BasicAnalyze.py
+++ src/BasicAnalyze.py
@@ -2,5 +2,24 @@ from src.util.Suit import Suit
 from src.util.WaitingStructure import WaitingStructure
 from src.util.WaitingType import WaitingType
 
+def getTempaiCount(suits, num: int) -> int:
+    tempaiCount = 0
+    for suit, waitingStructure in suits.items():
+        if suit.sum() != num:
+            continue
+
+        for tile, waitingType in zip(suit.suit, waitingStructure.waitingStructures):
+            if tile < suit.MAX_TILE_COUNT and waitingType.hasWaiting():
+                tempaiCount += 1
+                break
+
+    return tempaiCount
+
 def main(records):
-    suits = [{'suit': Suit(record['suit']), 'waitingStructure': WaitingStructure.getWaitingStructureFromString(record['waitingStructure'])} for record in records]
+    suits = {}
+    for record in records:
+        suits[Suit(record['suit'])] = WaitingStructure.getWaitingStructureFromString(record['waitingStructure'])
+
+    nums = [1, 2, 4, 5, 7, 8, 10, 11, 13]
+    for num in nums:
+        print(num, getTempaiCount(suits, num))
