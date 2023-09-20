06546990700650e86d72df2af9146798793d1930
firedial <42674772+firedial@users.noreply.github.com>
2023-09-20 12:06:23 +0900

    テストコードの追加

--- tests/test_Suit.py
+++ tests/test_Suit.py
@@ -34,19 +34,101 @@ class TestSuit(unittest.TestCase):
         suit = Suit((1, 0, 3, 0, 0, 0, 2, 1, 1))
         self.assertEqual(suit.sum(), 8)
 
+    def test_isTempai(self):
+        suit = Suit((2, 2, 2, 2, 0, 0, 1, 1, 1))
+        self.assertTrue(suit.isTempai())
+
+        suit = Suit((4, 0, 0, 0, 0, 0, 0, 0, 0))
+        self.assertFalse(suit.isTempai())
+
+    def test_isTempaiWithoutTileCount(self):
+        suit = Suit((2, 2, 2, 2, 0, 0, 1, 1, 1))
+        self.assertTrue(suit.isTempaiWithoutTileCount())
+
+        suit = Suit((4, 0, 0, 0, 0, 0, 0, 0, 0))
+        self.assertTrue(suit.isTempaiWithoutTileCount())
+
+    def test_isSendable(self):
+        suit = Suit((0, 0, 2, 2, 0, 0, 1, 1, 1))
+        self.assertFalse(suit.isSendable())
+
+        suit = Suit((0, 0, 2, 0, 0, 0, 0, 0, 0))
+        self.assertTrue(suit.isSendable())
+
+        suit = Suit((0, 0, 3, 1, 1, 0, 0, 0, 0))
+        self.assertTrue(suit.isSendable())
+
+        suit = Suit((0, 0, 3, 3, 2, 0, 0, 0, 0))
+        self.assertTrue(suit.isSendable())
+
+        suit = Suit((0, 3, 1, 1, 1, 1, 1, 0, 0))
+        self.assertTrue(suit.isSendable())
+
+        suit = Suit((0, 2, 2, 2, 2, 0, 0, 0, 0))
+        self.assertTrue(suit.isSendable())
+
+        suit = Suit((0, 1, 1, 4, 1, 1, 0, 0, 0))
+        self.assertTrue(suit.isSendable())
+
+        suit = Suit((0, 3, 3, 3, 1, 1, 0, 0, 0))
+        self.assertTrue(suit.isSendable())
+
+        suit = Suit((0, 2, 3, 3, 1, 1, 1, 0, 0))
+        self.assertTrue(suit.isSendable())
+
+        suit = Suit((0, 2, 2, 2, 3, 1, 1, 0, 0))
+        self.assertTrue(suit.isSendable())
+
+    def test_isRangeFull(self):
+        suit = Suit((0, 2, 2, 2, 3, 1, 1, 0, 0))
+        self.assertFalse(suit.isRangeFull())
+
+        suit = Suit((1, 2, 2, 2, 3, 1, 1, 2, 0))
+        self.assertFalse(suit.isRangeFull())
+
+        suit = Suit((1, 2, 2, 2, 3, 1, 1, 2, 2))
+        self.assertTrue(suit.isRangeFull())
+
+    def test_hasOneRoomRange(self):
+        suit = Suit((0, 2, 2, 2, 3, 1, 1, 0, 0))
+        self.assertFalse(suit.hasOneRoomRange())
+
+        suit = Suit((1, 2, 2, 2, 3, 1, 1, 2, 0))
+        self.assertTrue(suit.hasOneRoomRange())
+
+        suit = Suit((1, 2, 2, 2, 3, 1, 1, 2, 2))
+        self.assertFalse(suit.hasOneRoomRange())
+
+    def test_getLeftAttachSuit(self):
+        suit = Suit((0, 0, 2, 2, 3, 0, 0, 1, 0))
+        self.assertEqual(suit.getLeftAttachSuit(), Suit((2, 2, 3, 0, 0, 1, 0, 0, 0)))
+
+        suit = Suit((0, 2, 2, 2, 3, 0, 0, 0, 0))
+        self.assertEqual(suit.getLeftAttachSuit(), Suit((2, 2, 2, 3, 0, 0, 0, 0, 0)))
+
+    def test_getRightAttachSuit(self):
+        suit = Suit((0, 0, 2, 2, 3, 0, 0, 1, 0))
+        self.assertEqual(suit.getRightAttachSuit(), Suit((0, 0, 0, 2, 2, 3, 0, 0, 1)))
+
+        suit = Suit((0, 2, 2, 2, 3, 0, 0, 0, 0))
+        self.assertEqual(suit.getRightAttachSuit(), Suit((0, 0, 0, 0, 0, 2, 2, 2, 3)))
+
+    def test_getSuitNumber(self):
+        suit = Suit((0, 0, 2, 0, 0, 0, 0, 0, 0))
+        self.assertEqual(suit.getSuitNumber(), 78200000000)
+
+        suit = Suit((0, 2, 0, 0, 0, 0, 0, 0, 0))
+        self.assertEqual(suit.getSuitNumber(), 78200000000)
+
+        suit = Suit((0, 2, 2, 2, 2, 0, 0, 0, 0))
+        self.assertEqual(suit.getSuitNumber(), 34222200000)
+
     def test_isFirstTIleZero(self):
         suit = Suit((1, 2, 3, 4, 0, 0, 2, 0, 0))
         self.assertFalse(suit.isFirstTIleZero())
 
         suit = Suit((0, 2, 3, 4, 0, 0, 2, 0, 0))
         self.assertTrue(suit.isFirstTIleZero())
-
-    def test_getRightAttachSuit(self):
-        suit = Suit((0, 2, 3, 4, 0, 0, 2, 3, 0))
-        self.assertEqual(suit.getRightAttachSuit(), Suit((0, 0, 2, 3, 4, 0, 0, 2, 3)))
-
-        suit = Suit((0, 2, 3, 4, 0, 0, 0, 0, 0))
-        self.assertEqual(suit.getRightAttachSuit(), Suit((0, 0, 0, 0, 0, 0, 2, 3, 4)))
 
     def test_isBasicForm(self):
         suit = Suit((0, 2, 3, 4, 0, 0, 2, 3, 0))
